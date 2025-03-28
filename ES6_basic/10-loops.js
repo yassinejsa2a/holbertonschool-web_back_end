d88eb5d1ae8807ea2aedef8f4f5c589bf293d817
yassinejsa2a <9872@holbertonstudents.com>
2025-03-28 22:44:31 +0100

    updat bug

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for of (const value of array) {
-      const value;
-      array = appendString + value;
-    }
-  
-    return array;
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
   }
+
+  return result;
+}
--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments() {
-        return Object.keys(this.allEmployees).length;
-      },
-    };
-  }
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
