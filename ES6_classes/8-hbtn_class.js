ffebc40b7c8566299bf57c2b43b42b22b66d50cb
yassinejsa2a <yassinejsa@gmail.com>
2025-08-22 21:53:21 +0200

    feat: implement HolbertonClass with size and location properties

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,36 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = value;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+}
